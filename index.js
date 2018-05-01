dbad41494ae2c7ca258cd6a5f04608b3188daac4
Sean Kidwell <dev.seankidwell@gmail.com>
2018-05-01 02:56:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function theBeatlesPlay(arr1, arr2) {
+  var array = [];
+  for (var i = 0; i < arr1.length; i++) {
+    array.push(`${arr1[i]} plays ${arr2[i]}`)
+  }
+  return array;
+}
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+function johnLennonFacts(arr) {
+  var i = 0;
+  while (i < arr.length) {
+    
+  }
+}
